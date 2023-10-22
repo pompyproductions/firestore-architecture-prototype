9cacf3431d71103fda1d241a9bc79a722aae580a
Eren Gazioglu <pompyproduction@gmail.com>
2023-10-22 21:38:18 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import "./sass/styles.scss";
+import "./modules/resize";
+
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  // config goes here
+};
+const firebaseApp = initializeApp(firebaseConfig);
