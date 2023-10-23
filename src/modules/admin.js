c9ee45089b8c62f83bcdeab559c9ab70c86db102
Eren Gazioglu <pompyproduction@gmail.com>
2023-10-23 14:16:15 +0200

    Start working on auth

--- src/modules/admin.js
+++ src/modules/admin.js
@@ -1,2 +1,14 @@
 import "../sass/styles.scss";
 import "../modules/resize";
+
+import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "../config/firebaseConfig";
+
+const app = initializeApp("firebaseConfig");
+const auth = getAuth(app);
+
+signInWithEmailAndPassword(auth, email, password)
+  .then(userCred => {
+    const user = userCred.user
+  })
