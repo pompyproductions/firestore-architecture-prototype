2a56f1506a1dee5cc4321346af114d92eff4ce92
Eren Gazioglu <pompyproduction@gmail.com>
2023-10-24 00:20:49 +0200

    Write login logic

--- src/modules/admin.js
+++ src/modules/admin.js
@@ -1,14 +1,33 @@
 import "../sass/styles.scss";
 import "../modules/resize";
 
-import { getAuth, signInWithEmailAndPassword } from "firebase/auth";
+import { getAuth, onAuthStateChanged, signInWithEmailAndPassword } from "firebase/auth";
 import { initializeApp } from "firebase/app";
 import firebaseConfig from "../config/firebaseConfig";
 
-const app = initializeApp("firebaseConfig");
+const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
-signInWithEmailAndPassword(auth, email, password)
+onAuthStateChanged(auth, user => {
+  if (user) {
+    console.log(user.uid, "logged in")
+  } else {
+    console.log("not logged in")
+  }
+})
+
+document.getElementById("login-button").addEventListener("click", e => {
+  const email = document.getElementById("login-email").value;
+  const password = document.getElementById("login-password").value;
+
+  signInWithEmailAndPassword(auth, email, password)
   .then(userCred => {
-    const user = userCred.user
+    const user = userCred.user;
+    alert(`successfully logged in as: ${user}`);
   })
+  .catch(err => {
+    alert(err.message);
+    console.log(`error code: ${err.code}`);
+  })
+})
+
