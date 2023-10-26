d0f9d731cfbb0e939b539d24735a57132820fb89
Eren Gazioglu <pompyproduction@gmail.com>
2023-10-26 20:08:26 +0200

    Start writing auth module

--- src/modules/firebase/auth.js
+++ src/modules/firebase/auth.js
@@ -3,4 +3,23 @@ import { getAuth, onAuthStateChanged, signInWithEmailAndPassword, signOut } from
 
 const auth = getAuth(app);
 
-export default { }
+const logIn = (email, password) => {
+  const email = document.getElementById("login-email").value;
+  const password = document.getElementById("login-password").value;
+
+  signInWithEmailAndPassword(auth, email, password)
+  .then(userCred => {
+    const user = userCred.user;
+    alert(`successfully logged in as: ${user}`);
+  })
+  .catch(err => {
+    alert(err.message);
+    console.log(`error code: ${err.code}`);
+  })
+}
+
+const logOut = () => {
+  if (auth.currentUser) signOut(auth);
+}
+
+export default { logIn, logOut}
